9764f95f70f1886b1b74234d58b3481ea1202018
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-10-02 09:54:07 +0200

    chore: install and run prettier
    Related: #4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const createPlugin = (options) => {
-    return {
-        name: "@jop-software/astro-matomo",
-        hooks: {
-            'astro:config:setup': async ({ config, injectScript }) => {
-                injectScript('page', `import { init } from '@jop-software/astro-matomo/matomo.js'; init('${options.baseUrl}', ${options.siteId});`);
-            }
-        }
-    }
-}
+	return {
+		name: "@jop-software/astro-matomo",
+		hooks: {
+			"astro:config:setup": async ({ config, injectScript }) => {
+				injectScript(
+					"page",
+					`import { init } from '@jop-software/astro-matomo/matomo.js'; init('${options.baseUrl}', ${options.siteId});`
+				);
+			},
+		},
+	};
+};
 
 export default createPlugin;
--- src/matomo.js
+++ src/matomo.js
@@ -1,13 +1,17 @@
 export function init(baseUrl, siteId) {
-    var _paq = window._paq = window._paq || [];
-    /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-    _paq.push(['trackPageView']);
-    _paq.push(['enableLinkTracking']);
-    (function () {
-        var u = baseUrl;
-        _paq.push(['setTrackerUrl', u + 'matomo.php']);
-        _paq.push(['setSiteId', siteId]);
-        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-        g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
-    })();
+	var _paq = (window._paq = window._paq || []);
+	/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+	_paq.push(["trackPageView"]);
+	_paq.push(["enableLinkTracking"]);
+	(function () {
+		var u = baseUrl;
+		_paq.push(["setTrackerUrl", u + "matomo.php"]);
+		_paq.push(["setSiteId", siteId]);
+		var d = document,
+			g = d.createElement("script"),
+			s = d.getElementsByTagName("script")[0];
+		g.async = true;
+		g.src = u + "matomo.js";
+		s.parentNode.insertBefore(g, s);
+	})();
 }
